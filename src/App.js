557271115a968c678aa077abcb33d2604cdcf151
kodyimmink <kodyimmink@gmail.com>
2019-03-11 14:58:46 -0400

    added location getLocation component. brewit now places a marker on the map for the users location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+
 import SwipeableTemporaryDrawer from './components/SwipeableTemporaryDrawer';
+
 
 import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import {getLocation} from './components/getLocation';
+
+import breweryIconSvg from './icons/beer.svg';
+import userLocationSvg from './icons/userLocation.svg';
 
 import './App.css';
-import './'
 
 const openMapTiles = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+
+const breweryIcon = L.icon({
+  iconUrl: breweryIconSvg,
+  iconSize: [50, 82]
+});
+
+const userLocation = L.icon({
+  iconUrl: userLocationSvg,
+  iconSize: [50, 82]
+});
 
 
 class App extends Component {
@@ -16,12 +31,20 @@ class App extends Component {
       lat: 51.505,
       lng: -0.09,
     },
+    haveUsersLocation: false,
     zoom: 2
   }
 
-  /*componentDidMount() {
-    //TO-DO: navigator.geolocation.getCurrentPosition(pos=> position);
-  }*/
+  componentDidMount() {
+    getLocation()
+    .then(location => {
+      this.setState({
+        location,
+        haveUsersLocation: true,
+        zoom: 13
+      });
+    });
+  }
 
   render() {
     const position = [this.state.location.lat, this.state.location.lng];
@@ -38,6 +61,13 @@ class App extends Component {
             url={openMapTiles}
           />
           <SwipeableTemporaryDrawer></SwipeableTemporaryDrawer>
+          {
+            this.state.haveUsersLocation ? 
+            <Marker
+              position={position}
+              icon={userLocation}>
+            </Marker> : ''
+          }
         </Map>     
       </div>
     );
